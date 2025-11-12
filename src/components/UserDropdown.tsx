38a3cecba6ec5069254af9452c5b33e3bee65b5a
Kritika Singh <kritikasingh00.com@gmail.com>
2025-11-12 21:48:23 +0530

    add header

---
+++ src/components/UserDropdown.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { LogOut } from "lucide-react";
+import NavItems from "@/components/NavItems";
+
+const UserDropdown = () => {
+  const router = useRouter();
+
+  const handleSignOut = async () => {
+    router.push("/sign-in");
+  };
+
+  const user = { name: "ojha", email: "contact@ojha.com" };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="ghost"
+          className="flex items-center gap-3 text-gray-400 hover:text-yellow-500"
+        >
+          <Avatar className="h-8 w-8">
+            <AvatarImage src="https://imgs.search.brave.com/QJbiNRIjqzDXpRuYueFUYyKDCZRZFIri-l0coQMK8Xc/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9paDEu/cmVkYnViYmxlLm5l/dC9pbWFnZS41Njcx/MjI3NTM5LjAwMjEv/c3Qsc21hbGwsNTA3/eDUwNy1wYWQsNjAw/eDYwMCxmOGY4Zjgu/anBn" />
+            <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">
+              {user.name[0]}
+            </AvatarFallback>
+          </Avatar>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="text-gray-400">
+        <DropdownMenuLabel>
+          <div className="flex relative items-center gap-3 py-2">
+            <Avatar className="h-10 w-10">
+              <AvatarImage src="https://imgs.search.brave.com/QJbiNRIjqzDXpRuYueFUYyKDCZRZFIri-l0coQMK8Xc/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9paDEu/cmVkYnViYmxlLm5l/dC9pbWFnZS41Njcx/MjI3NTM5LjAwMjEv/c3Qsc21hbGwsNTA3/eDUwNy1wYWQsNjAw/eDYwMCxmOGY4Zjgu/anBn" />
+              <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">
+                {user.name[0]}
+              </AvatarFallback>
+            </Avatar>
+
+            <div className="flex flex-col">
+              <span className="text-base font-medium text-gray-400">
+                {user.name}
+              </span>
+              <span className="text-gray-500 text-sm">{user.email}</span>
+            </div>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator className="bg-gray-600" />
+        <DropdownMenuItem
+          onClick={handleSignOut}
+          className="text-mg text-gray-100 font-medium focus:text-yellow-500 transition-colors cursor-pointer"
+        >
+          <LogOut className="h-4 w-4 mr-2 hidden sm:block" />
+          Sign Out
+        </DropdownMenuItem>
+        <DropdownMenuSeparator className="hidden sm:blockbg-gray-600" />
+
+        <nav className="sm:hidden">
+          <NavItems />
+        </nav>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+export default UserDropdown;

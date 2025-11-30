ace334d199407765c416903ee55ecb26b9a81366
Kritika Singh <kritikasingh00.com@gmail.com>
2025-11-30 21:53:36 +0530

    implement search stocks

--- src/components/UserDropdown.tsx
+++ src/components/UserDropdown.tsx
@@ -16,7 +16,13 @@ import NavItems from "@/components/NavItems";
 import { signOut } from "@/lib/actions/auth.actions";
 import { toast } from "sonner";
 
-const UserDropdown = ({ user }: { user: User }) => {
+const UserDropdown = ({
+  user,
+  initialStocks,
+}: {
+  user: User;
+  initialStocks: StockWithWatchlistStatus[];
+}) => {
   const router = useRouter();
 
   const handleSignOut = async () => {
@@ -73,7 +79,7 @@ const UserDropdown = ({ user }: { user: User }) => {
         <DropdownMenuSeparator className="hidden sm:block bg-gray-600" />
 
         <nav className="sm:hidden">
-          <NavItems />
+          <NavItems initialStocks={initialStocks} />
         </nav>
       </DropdownMenuContent>
     </DropdownMenu>
